cc696a6eb0a38175ec82db918d862fdc73b337d0
taxuanthang <thangtaxuan65@gmail.com>
2024-11-29 00:17:45 +0700

    make the sheep spawn

--- Assets/Sheep.cs
+++ Assets/Sheep.cs
@@ -1,18 +1,3 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Sheep : MonoBehaviour
-{
-    public GameObject sheepGameObject { set; get; }
-    public Rigidbody2D rb { set; get; }
-    public int number { set; get; }
-    public int runspeed { set; get; }
-    public Sheep(GameObject sheepObject, int number, int runspeed)
-    {
-        this.sheepGameObject = sheepObject;
-        this.number = number;
-        this.runspeed = runspeed;
-        rb = sheepObject.GetComponent<Rigidbody2D>();
-    }
-}
--- Assets/SheepHandler.cs
+++ Assets/SheepHandler.cs
@@ -29,58 +29,116 @@ using static UnityEditor.Timeline.TimelinePlaybackControls;
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //            Phật phù hộ, không bao giờ BUG
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+[System.Serializable]
+public class Sheep 
+{
+    public GameObject sheepsPrefab;
+    public GameObject sheepGameObject { set; get; }
+    private Rigidbody2D rb;
+    private Collider2D Collider2D;
+    public int number;
+    public int runspeed;
+    public int mass;
+    public Vector2 bornPos;
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Debug.Log(collision.name);
+    //}
+    public void BornSheep()
+    {
+        sheepGameObject = GameObject.Instantiate(sheepsPrefab, bornPos, Quaternion.identity);
+        this.rb = sheepGameObject.GetComponent<Rigidbody2D>();
+        this.rb.mass = this.mass;
+        this.Collider2D = this.sheepGameObject.GetComponent<Collider2D>();
+    }
+    public void Jump()
+    {
+        this.rb.AddForce(Vector2.up * this.runspeed, ForceMode2D.Impulse);
+    }
+    public void Move(string dir)
+    {
+        Vector2 vectorDir = Vector2.zero;
+        if (dir == "right")
+        {
+            vectorDir = Vector2.right;
+        }
+        if (dir == "left")
+        {
+            vectorDir = Vector2.left;
+        }
+        if (Mathf.Abs(this.rb.velocity.x) < this.runspeed)
+        {
+            this.rb.AddForce(vectorDir * this.runspeed / 2, ForceMode2D.Force);
+        }
+    }
+
+
+}
+
 public class SheepHandler : MonoBehaviour
 {
-    public GameObject cube;
-    [SerializeField] List<GameObject> sheepsPrefab;
-    private List<Sheep> sheeps;
+    public List<Sheep> sheeps;
     private Sheep targetSheep;
     public GameObject sheepArrow;
     UnityEngine.Vector2 vectorDir = new UnityEngine.Vector2(0,0);
 
+    private bool isJumped;
     private void Awake()
     {
-        sheeps = new List<Sheep>() 
+        //sinh cuu
+        foreach (var sheep in sheeps)
         {
-            new(Instantiate(sheepsPrefab[0],new Vector2(1,0),Quaternion.identity),0,10),
-            new(Instantiate(sheepsPrefab[1],new Vector2(1,0),Quaternion.identity),1,11),
-            new(Instantiate(sheepsPrefab[2],new Vector2(1,0),Quaternion.identity),2,12),
-        };
-        
+            sheep.BornSheep();
+        }
+
+        //gan cuu
         targetSheep = sheeps[0];
 
     }
-    public void CharterControl()
+
+ 
+    public void CharterInputControl()
     {
-        //if (Input.GetKeyDown(KeyCode.W))
-        //{
-        //    vectorDir = new UnityEngine.Vector2(1, 0);
-        //}
-        if (Input.GetKeyDown(KeyCode.A))
+        //Moving Left Right
+        if (Input.GetKey(KeyCode.A))
+            {
+                targetSheep.Move("left");
+            }
+        if (Input.GetKey(KeyCode.D))
+            {
+                targetSheep.Move("right");
+            }
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.W))
         {
-            vectorDir = new UnityEngine.Vector2(-1, 0);
-            targetSheep.rb.AddForce(targetSheep.runspeed * vectorDir, ForceMode2D.Impulse);
-            print(vectorDir);
+            targetSheep.Jump();
         }
-        //if (Input.GetKeyDown(KeyCode.S))
-        //{
-        //    vectorDir = new UnityEngine.Vector2(1, 0);
-        //}
-        if (Input.GetKeyDown(KeyCode.D))
+
+        //Switching sheep
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            vectorDir = new UnityEngine.Vector2(1, 0);
-            targetSheep.rb.AddForce(targetSheep.runspeed * vectorDir, ForceMode2D.Impulse);
+            targetSheep = sheeps[0];
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            vectorDir = new UnityEngine.Vector2(0, 1);
-            targetSheep.rb.AddForce(targetSheep.runspeed * vectorDir, ForceMode2D.Impulse);
+            targetSheep = sheeps[1];
         }
-        
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            targetSheep = sheeps[2];
+        }
+
     }
+
+    
     private void Update()
     {
-        CharterControl();
-        print(targetSheep.sheepGameObject.transform.position);
+        CharterInputControl();
+        //dieu khien cuu duoc chon
+        
+        //print(targetSheep.sheepGameObject.transform.position);
     }
 }
